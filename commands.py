bcf7bc12a0292eb223927a34e8bed644fb361f96
GuineaMan <blaze420@cock.li>
2015-08-25 23:24:44 +0000

    First commit

--- commands.py
+++ commands.py
@@ -86,91 +86,14 @@ def get_command(name):
     else:
         return nothing
 
-
-def twitter(args):
-    print args
-    if type(args) is not str:
-        tweet = " ".join(args["args"])
-        sendmsg = args["sendmsg"]
-        channel = args["channel"]
-    else:
-        tweet = args
-    r = requests.post("http://carta.im/tweetproxy/", data={'tweet': tweet})
-    if "200" in r.text:
-        return ":DDD brought to you by @proxytwt"
-    else:
-        return ":( pls fix me ;-;"
-
-
-@command("tweet")
-def tweet(args):
-    return twitter(args)
-
-@command("shitpost")
-def shitposting(args):  # almost entirely automated shitposting
-    if " ".join(args["args"]) == "| tweet":
-        directory = os.path.dirname(__file__)
-        with open(directory + "/shitpost.txt", 'r') as twit:
-            shitpost = twit.read()[0:140]
-        return twitter(shitpost)
-
-    shitpost = fourchan_json.get_random_post(args)
-    shitpost = shitpost.split("\x0f")
-    res_shitpost = []
-    for l in shitpost:
-        if l.strip().startswith(">"):
-            l = tcol.DARK_GREEN + l
-        res_shitpost.append(l)
-    shitpost = "\x0f".join(res_shitpost)
-
-    if args["command"].isupper():
-        shitpost = shitpost.upper()
-    directory = os.path.dirname(__file__)
-    with open(directory + "/shitpost.txt", 'w') as tweet:
-        tweet.write(shitpost)
-    return shitpost
-
-
 @command("cybhelp")
 def halp(args):
     user = getuser(args["raw"])
     string = user + ", sending you a private message of my commands.\n"
-    args["sendmsg"](user, "ur a faget")
+    args["sendmsg"](user, "commands are: eightball guinea cute cybhelp booty shrug denko")
     return string
 
 
-@command("interject")
-def interjection(args):  # I'd just like to interject for a moment
-    str = ("I'd just like to interject for moment. What you're referring to as "
-              "Linux, is in fact, GNU/Linux, or as I've recently taken to calling it,"
-              " GNU plus Linux. pastebin.com/2YxSM4St\n")
-    return str
-
-
-@command("git")
-def git(args):
-    str = "https://github.com/cybits/cybot What are we going to do on the repo? waaaah fork =3\n"
-    return str
-@command("reminder")
-def reminder(args):  # today, I will remind them
-    return ('Remember to fix your posture :D http://gateway.ipfs.io/ipfs/QmR91KZ77KMg1h8HqBTxtHNZiWCECiXhHkTR7coVqyyFvF/posture.jpg\n')
-
-@command("memearrows")
-def memearrows(args):  # >implying you can triforce
-    str = ("Meme arrows are often used to preface implications or feels. See "
-              "also: implying, feel.\n")
-    return str
-
-
-@command("int")
-def intensifies(args):  # [python intensifies]
-    if args["args"]:
-        ret = "[" + " ".join(args["args"]) + " intensifies]\n"
-    else:
-        ret = "[no argument intensifies]\n"
-    if args["command"].isupper():
-        return ret.upper()
-    else: return ret
 
 
 # TODO: Use for something
@@ -180,76 +103,7 @@ def hello(user):  # This function responds to a user that inputs "Hello cybits"
     return str
 
 
-@command("ayylmao")
-def ayylmao(args): 
-    import time
-    sendmsg = args["sendmsg"]
-    line = ('ABDUCTION: INCOMING')
-    
-    ayylien = ["       .-""""-.        .-""""-.    ",
-               "      /        \      /        \   ",
-               "     /_        _\    /_        _\  ",
-               "    // \      / \\  // \      / \\ ",
-               "    |\__\    /__/|  |\__\    /__/| ",
-               "     \    ||    /    \    ||    /  ",
-               "      \        /      \        /   ",
-               "       \  __  /        \  __  /    ",
-               "        '.__.' ayy lmao '.__.'     "]
-    
-    
-    ircmsg = args["raw"]
-    user = ircmsg.split(":")[1].split('!')[0]
-    channel = args["channel"]
-    sendmsg(channel, line)
-    for lines in ayylien:
-	sendmsg(user, lines)
-	time.sleep(1)
-   # ayy lmao 
-   # Doing all the logic inside the function
-   # Since sendmsg wont post empty strings.
-    return ""
 
-@command("feel")
-def feel(args):  # >tfw
-    import time
-    sendmsg = args["sendmsg"]
-    line = ('"tfw no gf" is an abbreviated expression for "that feeling [I get] '
-              'when [I have] no girlfriend" often used in online discussions and '
-              'comments.')
-
-    feelguy  = ["░░░░░░░▄▀▀▀▀▀▀▀▀▀▀▄▄░░░░░░░░░",
-                "░░░░▄▀▀░░░░░░░░░░░░░▀▄░░░░░░░",
-                "░░▄▀░░░░░░░░░░░░░░░░░░▀▄░░░░░",
-                "░░█░░░░░░░░░░░░░░░░░░░░░▀▄░░░",
-                "░▐▌░░░░░░░░▄▄▄▄▄▄▄░░░░░░░▐▌░░",
-                "░█░░░░░░░░░░░▄▄▄▄░░▀▀▀▀▀░░█░░",
-                "▐▌░░░░░░░▀▀▀▀░░░░░▀▀▀▀▀░░░▐▌░",
-                "█░░░░░░░░░▄▄▀▀▀▀▀░░░░▀▀▀▀▄░█░",
-                "█░░░░░░░░░░░░░░░░▀░░░▐░░░░░▐▌",
-                "▐▌░░░░░░░░░▐▀▀██▄░░░░░░▄▄▄░▐▌",
-                "░█░░░░░░░░░░░▀▀▀░░░░░░▀▀██░▀▄",
-                "░▐▌░░░░▄░░░░░░░░░░░░░▌░░░░░░█",
-                "░░▐▌░░▐░░░░░░░░░░░░░░▀▄░░░░░█",
-                "░░░█░░░▌░░░░░░░░▐▀░░░░▄▀░░░▐▌",
-                "░░░▐▌░░▀▄░░░░░░░░▀░▀░▀▀░░░▄▀░",
-                "░░░▐▌░░▐▀▄░░░░░░░░░░░░░░░░█░░",
-                "░░░▐▌░░░▌░▀▄░░░░▀▀▀▀▀▀░░░█░░░",
-                "░░░█░░░▀░░░░▀▄░░░░░░░░░░▄▀░░░",
-                "░░▐▌░░░░░░░░░░▀▄░░░░░░▄▀░░░░░",
-                "░▄▀░░░▄▀░░░░░░░░▀▀▀▀█▀░░░░░░░",
-                "▀░░░▄▀░░░░░░░░░░▀░░░▀▀▀▀▄▄▄▄▄"]
-
-
-    ircmsg = args["raw"]
-    user = ircmsg.split(":")[1].split('!')[0]
-    channel = args["channel"]
-    sendmsg(channel, line)
-    for lines in feelguy:
-        sendmsg(user, lines)
-        time.sleep(1)
-    # Doing all the logic inside the function
-    # Since sendmsg wont post empty strings.
-    return ""
 
 
 # TODO: Use this for something
@@ -279,56 +133,20 @@ def autointerject(args):  # making sure users don't forget the GNU
     return str1, str2
 
 
-@command("implying")
-def implying(args):  # >implying this needs a comment
-    return ('>implying is used in a mocking manner to challenge an "implication" '
-            'that has been made, or sometimes it can be simply used as a joke in '
-            'itself.\n')
 
-
-@command("lit")
-def sentence(args):  # This function grabs a random sentence from a txt file and posts it to the channel
-    directory = os.path.dirname(__file__)
-    directory = directory + os.path.join("/texts/books/")
-    line = directory + os.path.join(random.choice(os.listdir(directory)))
-    return get_random_line(line) + "\n"
-    # return get_random_line(random.choice(os.listdir("/home/polaris/PycharmProjects/cybot/texts/"))) + "\n"
 
 @command("guinea")
 def guinea(args):
     directory = os.path.dirname(__file__)	
     guinea = directory + os.path.join("/texts/other/guinea.txt")
     return random.choice(list(open(guinea)))
-    
-@command("terry")
-def terry(args):  # Grabs a random Terry quote from the 9front list
-    directory = os.path.dirname(__file__)
-    terry = directory + os.path.join("/texts/other/terry.txt")
-    return random.choice(list(open(terry)))
 
-@command("rob")
-def pike(args):
-    directory = os.path.dirname(__file__)
-    pike = directory + os.path.join("/texts/other/rob.txt")
-    return random.choice(list(open(pike)))
 
-@command("gene")
-def ray(args):
-    directory = os.path.dirname(__file__)
-    ray = directory + os.path.join("/texts/other/timecube.txt")
-    return random.choice(list(open(ray)))
-    
-@command("linus")
-def torv(args):
-    directory = os.path.dirname(__file__)
-    torv = directory + os.path.join("/texts/other/linus.txt")
-    return random.choice(list(open(torv)))
-
-@command("rms")
-def stallman(args):
-    directory = os.path.dirname(__file__)
-    richard = directory + os.path.join("/texts/other/stallman.txt")
-    return random.choice(list(open(richard)))
+@command("facts")
+def facts(args):
+    directory = os.path.dirname(__file__)	
+    facts = directory + os.path.join("/texts/other/facts.txt")
+    return random.choice(list(open(facts)))    
 
 @command("eightball")
 def eight(args):
@@ -336,14 +154,7 @@ def eight(args):
     eight = directory + os.path.join("/texts/other/eightball.txt")
     return random.choice(list(open(eight)))
 
-@command("triforce")
-def coolt(args):
-    sendmsg = args["sendmsg"]
-    spaces1 = random.randint(1,5)
-    spaces2 = random.randint(1,3)
-    string1 = (" "*spaces1 + (u"▲").encode('utf-8'))
-    string2 = (" "*spaces2 + (u"▲ ▲").encode('utf-8'))
-    return string1, string2
+
 
 
 @command("booty")
@@ -392,32 +203,7 @@ def cute(args):
 def bots(args):
     return "Reporting in! [Python] Try .cybhelp for commands."
     
-@command("valka")
-def valka(args):
-    """We miss you"""
-    return "..."
-    
-@command("hackers")
-def hackers(args):
-    directory = os.path.dirname(__file__)
-    hackers = directory + os.path.join("/texts/other/hackers.txt")
-    return random.choice(list(open(hackers)))
 
-@command("noided")
-def noided(args):
-    directory = os.path.dirname(__file__)
-    noided = directory + os.path.join("/texts/other/grips.txt")
-    return random.choice(list(open(noided)))
-   
-@command("just")
-def just(args):
-	return "DO IT", "JUST DO IT", "DON'T LET YOUR DREAMS BE DREAMS", "YESTERDAY YOU SAID TOMORROW", "SO JUST-", "DO IT", "MAKE YOUR DREAMS COME TRUE", "JUST-DO IT", "YES YOU CAN", "JUST DO IT", "If you are tired of starting over, stop giving up", "*flexes*"
-
-@command("spooky")
-def spooky(args):
-    directory = os.path.dirname(__file__)
-    spook = directory + os.path.join("/texts/other/spooks")
-    return random.choice(list(open(spook)))
    
 def breaklines(str):  # This function breaks lines at \n and sends the split lines to where they need to go
     strarray = string.split(str, "\n")
